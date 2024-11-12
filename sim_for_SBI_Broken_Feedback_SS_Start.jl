68336a9e6e7ce2533e2a350e1ad998c05a95e638
Helmut Strey <Helmut.Strey@stonybrook.edu>
2024-11-12 11:37:35 -0500

    made some changes to distributions Normal -> Gamma

--- sim_for_SBI_Broken_Feedback_SS_Start.jl
+++ sim_for_SBI_Broken_Feedback_SS_Start.jl
@@ -1,4 +1,4 @@
-using Catalyst, DifferentialEquations, Plots, Interpolations
+using Catalyst, Plots, Interpolations, JumpProcesses
 using Statistics, Distributions, StatsBase
 using CSV, DataFrames, FFTW, DSP
 using SymbolicIndexingInterface: parameter_values, state_values
@@ -8,22 +8,24 @@ using JLD2, UnPack
 @parameters kOn, kOff, kteton, ktetoff, kTr, kTl, dM, dG;
 @variables t;
 @species A(t), DNAoff(t), DNAon(t), RNA(t), GFP(t);
-u0 =()
+
 # Initial conditions and base parameters
-tspan = (0.0, 2000);
+tspan = (0.0, 500);
 u0 = (DNAoff => 1, DNAon => 0, A => 1, RNA => 35, GFP => 2800000); #Changed to approximately level of GFP and RNA at steady state
 #base_kteton = 0.0007
 
-# Define the different factors for kteton
-kteton_factors=[1]
-#kteton_factors = [1, 0.75, 0.5, 0.25, 0.1];
-p=()
-#p = (kOn => 0.00042*60*60, kOff => 0.0042*60*60, kteton => 0.0001016273663, ktetoff => 54, kTr => 375, kTl => 310, dM => 1, dG => 0.00365)
 p = (kOn => 0.00042*60*60, kOff => 0.0042*60*60,  kTr => 375, kTl => 310, dM => 1, dG => 0.00365) #parameter set for NO FEEDBACK Circuit
 ukOn = Normal(0.00042*60*60,0.2*0.00042*60*60) # standard deviation raised to 20%. estimated from Suter et al. paper that used/measured kon and koff values (no reported standard deviation)
 ukOff = Normal(0.0042*60*60,0.2*0.0042*60*60)
 ukTr = Normal(375,125)
-ukTl = Normal(310,265)
+# calculate gamma distribution for kTL with mean=310 and std=265
+g_scale = 265^2/310
+g_shape = 310/g_scale
+ukTl = Gamma(g_shape,g_scale)
+
+kTl_x = 0:01:600
+kTl_y = pdf.(ukTl,kTl_x)
+plot(kTl_x, kTl_y)
 
 # Define the reaction network
 rxs = [
@@ -47,13 +49,13 @@ jprob = JumpProblem(rn, dprob, Direct(); save_positions = (false, false));
 states = unknowns(rn)
 params = parameters(rn)
 
-plot(sol, idxs=5, label="GFP",xlims=(0,2000))
+plot(sol, idxs=5, label="GFP")
 #plot(sol, idxs=4, label="RNA",xlims=(0,50))
 
 plot(sol, idxs=3, label="DNAon",xlims=(0,2000))
 savefig("newpara.png")
 
-GFP = sol[5,:][3000:3215]/10000 #Changed to get values from indexes corresponding to past 1000hrs (steady state) now that there are 3x as many points saved in simulation output
+GFP = sol[5,:][1288:1503]/1000000 #Changed to get values from indexes corresponding to past 1000hrs (steady state) now that there are 3x as many points saved in simulation output
 plot(GFP) #look at stored GFP trace over this time period
 k1 = mean(GFP)
 k2 = var(GFP)
@@ -71,7 +73,7 @@ plot(pw.power[2:end])
 
 sumstats_list = Vector{Float64}[]
 params_list = Vector{Float64}[]
-@time for i in 1:100
+@time for i in 1:50000
     if (i % 100)==0
         println(i)
     end 
@@ -83,9 +85,10 @@ params_list = Vector{Float64}[]
                             kOff => kOff_new,
                             kTr => kTr_new,
                             kTl => kTl_new))
-    jprob = JumpProblem(rn, dprob, Direct(); save_positions = (false, false));
+    jprob = JumpProblem(rn, new_prob, Direct(); save_positions = (false, false));
     sol = solve(jprob, SSAStepper(); saveat=0.333) #changed to 0.333 hrs to match measured data
-    GFP = sol[5,:][3000:3215]/10000 #Changed to get values from indexes corresponding to past 1000hrs (steady state) now that there are 3x as many points saved in simulation output (only storing 216 data points) 
+    GFP = sol[5,:][1288:1503]/1000000 #Changed to get values from indexes corresponding to past 1000hrs (steady state) now that there are 3x as many points saved in simulation output (only storing 216 data points) 
+    #RNA = sol[4,:][1288:1503]
 
     k1 = mean(GFP)
     k2 = var(GFP)
@@ -93,6 +96,7 @@ params_list = Vector{Float64}[]
     k4 = mean((GFP.-k1).^4) .- 3*k2^2
     k5 = mean((GFP.-k1).^5) - 10*k3*k2
 
+    #println(k1," ",mean(RNA))
     pw = welch_pgram(GFP)
     ps = pw.power[2:11]
     params = Float64[ kOn_new, kOff_new, kTr_new, kTl_new]
