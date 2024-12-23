9737c9713db7777ba9cb87161fb846b359d3bc37
Helmut Strey <Helmut.Strey@stonybrook.edu>
2024-12-23 12:30:18 -0500

    changes to get proper samples for SBI

--- ForHelmut_Sim_Broken_Feedback_Circuit_12_3.jl
+++ ForHelmut_Sim_Broken_Feedback_Circuit_12_3.jl
@@ -13,9 +13,10 @@ using Polynomials
 
 # Initial conditions and base parameters
 tspan = (0.0, 300);
-u0 = (DNAoff => 1, DNAon => 0, A => 1, RNA => 147, GFP => 1.7098053e7); #Changed to approximately level of GFP and RNA at steady state
+u0 = [DNAoff => 1, DNAon => 0, A => 1, RNA => 0, GFP => 0]; #Changed to approximately level of GFP and RNA at steady state
 
 
+#p = [kOn => 0.00042*60*60, kOff => 0.0042*60*60,  kTr => 310, kTl => 425, dM => 1, dG => 0.00365] #parameter set for NO FEEDBACK Circuit
 p = (kOn => 0.00009*60*60, kOff => 0.0001*60*60,  kTr => 310, kTl => 425, dM => 1, dG => 0.00365) #parameter set for NO FEEDBACK Circuit
 ukOn = Normal(0.00009*60*60,0.2*0.00009*60*60) # standard deviation raised to 20%. estimated from Suter et al. paper that used/measured kon and koff values (no reported standard deviation)
 ukOff = Normal(0.0001*60*60,0.2*0.0001*60*60)
@@ -31,10 +32,10 @@ max_value=[]
 max_value = 575
 
 # Function to sample with a maximum value
-function bounded_sample(distribution, max_value)
+function bounded_sample(distribution, max_value, min_value)
     while true
         sample = rand(distribution)
-        if sample <= max_value
+        if min_value <= sample <= max_value
             return sample
         end
     end
@@ -43,14 +44,21 @@ end
 dist_b_kTl=[]
 for i in 1:1000
     # Generate a new sample
-    kTl_new = bounded_sample(ukTl, max_value)
+    kTl_new = bounded_sample(ukTl, max_value, 100)
     push!(dist_b_kTl,kTl_new)
+end
+dist_b_kTr=[]
+for i in 1:1000
+    # Generate a new sample
+    kTr_new = bounded_sample(ukTr, max_value, 50)
+    push!(dist_b_kTr,kTr_new)
 end
 #visuallize the distribution sampled from and the resultant distribution of selected kTl values, representative of what will occur during the simulation loop
 kTl_x = 0:01:1000
 kTl_y = pdf.(ukTl,kTl_x)
 plot(kTl_x, kTl_y)
 histogram(dist_b_kTl,bins=200)
+histogram(dist_b_kTr,bins=200)
 # Define the reaction network
 rxs = [
     (@reaction kOn, DNAoff + A --> DNAon),
@@ -68,7 +76,7 @@ rn = complete(rn);
 # Define and solve the problem
 dprob = DiscreteProblem(rn, u0, tspan, p);
 jprob = JumpProblem(rn, dprob, Direct(); save_positions = (false, false));
-@time sol = solve(jprob, SSAStepper(); saveat=0.333);
+@time sol = solve(jprob, SSAStepper(); saveat=0.02);
 
 states = unknowns(rn)
 params = parameters(rn)
@@ -77,32 +85,32 @@ GFP1 = sol[5,:][end-215:end]/1000000
 #plot GFP and RNA values of the simulation
 plot(sol, idxs=5, label="GFP")
 plot(GFP1)
-plot(sol, idxs=4,label="RNA")
-
+plot(sol, idxs=3,label="Activator",xlim=(0,20),xlabel="t in hours")
+savefig("activator.png")
 
 #Setup for simulation loop, parameters, summary statistics, and section of GFP time trace at steady state are stored during the loop
 sim_sumstats_list = Vector{Float64}[]
 sim_params_list = Vector{Float64}[]
 sim_GFP=Vector{Float64}[]
-@time for i in 1:50000
+@time for i in 1:49800
     if (i % 100)==0
         println(i)
     end 
     kOn_new = rand(ukOn)
     kOff_new = rand(ukOff)
-    kTr_new = rand(ukTr)
-    kTl_new = bounded_sample(ukTl, max_value)
+    kTr_new = bounded_sample(ukTr, 600, 50)
+    kTl_new = bounded_sample(ukTl, max_value, 100)
     u0RNA = kOn_new/(kOn_new+kOff_new)*kTr_new
     u0GFP = kTl_new*u0RNA/0.00365
-    new_prob = remake(dprob; p = (kOn => kOn_new,
+    new_prob = remake(dprob; p = [kOn => kOn_new,
                             kOff => kOff_new,
                             kTr => kTr_new,
-                            kTl => kTl_new),
-                            u0 = (RNA => Int64(round(u0RNA)), GFP => Int64(round(u0GFP))),
-                            tspan = (0.0,150.0))
+                            kTl => kTl_new],
+                            u0 = [DNAoff => 1, DNAon => 0, A => 1, RNA => u0RNA, GFP => u0GFP],
+                            tspan = (0.0,300.0))
     jprob = JumpProblem(rn, new_prob, Direct(); save_positions = (false, false));
     sol = solve(jprob, SSAStepper(); saveat=1/3) #changed to 0.333 hrs to match measured data
-    GFP = sol[5,:][end-215:end]/1000000 #Changed to get values from indexes corresponding to past 1000hrs (steady state) now that there are 3x as many points saved in simulation output (only storing 216 data points) 
+    GFP = sol[5,end-215:end]/1000000 #Changed to get values from indexes corresponding to past 1000hrs (steady state) now that there are 3x as many points saved in simulation output (only storing 216 data points) 
     
     k1 = mean(GFP)
     k2 = var(GFP)
@@ -122,13 +130,14 @@ end
 
 plot!(legend=:false)
 plot(sim_GFP,legend=:false,xlabel="time points",ylabel="#GFP Molecules (10^6)")
+savefig("GFPPlots.png")
 
 sim_sumstats=[];
 sim_sumstats = reduce(vcat,transpose.(sim_sumstats_list))
 params = reduce(vcat,transpose.(sim_params_list))
 sec_sim_traces = DataFrame([Symbol("Col$i") => vec for (i, vec) in enumerate(sim_GFP)])
 #CSV.write("TestName_traces.csv", sec_sim_traces)
-jldsave("run3bfl.jld2"; sim_sumstats,params)
+jldsave("run1bfc.jld2"; sim_sumstats,params)
 
 # assemble all the simulations
 @unpack sim_sumstats,params = jldopen("run1bfl.jld2")
@@ -151,3 +160,22 @@ jldsave("bfc.jld2"; sumstatst,paramst)
 # now add slope as another summary statistics
 @unpack sumstatst,paramst = jldopen("bfc.jld2")
 
+@parameters kOn, kOff, kteton, ktetoff, kTr, kTl, dM, dG;
+@variables t;
+@species A(t), DNAoff(t), DNAon(t), RNA(t), GFP(t);
+rxs = [
+    (@reaction kOn, DNAoff + A --> DNAon),
+    (@reaction kOff, DNAon --> DNAoff + A),
+    (@reaction kTr, DNAon --> DNAon + RNA),
+    (@reaction kTl, RNA --> RNA + GFP),
+    (@reaction dM, RNA --> 0),
+    (@reaction dG, GFP --> 0),
+];
+
+@named rn = ReactionSystem(rxs, t, [A, DNAoff, DNAon, RNA, GFP], [kOn, kOff, kTr, kTl, dM, dG]);
+rn = complete(rn);
+
+# Define and solve the problem
+dprob = DiscreteProblem(rn, u0, tspan, p);
+jprob = JumpProblem(rn, dprob, Direct(); save_positions = (false, false));
+@time sol = solve(jprob, SSAStepper(); saveat=0.333);
